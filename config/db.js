942bffd649ed312b2d92fbe3e1d93485ffcf1eca
Nikhil Tripathi <nikhiltr04@gmail.com>
2025-08-20 12:29:09 +0530

    increased connecttimeout

--- config/db.js
+++ config/db.js
@@ -7,9 +7,12 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // Make sure port is included
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  // Add this line to increase the timeout
+  connectTimeout: 30000
 });
 
 module.exports = pool.promise();
